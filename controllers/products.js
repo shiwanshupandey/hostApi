c53fb95681710cfe294ce4944216046a1eba196c
shiwanshu pandey <48207289+shiwanshupandey@users.noreply.github.com>
2023-08-17 16:17:49 +0530

    Update products.js

--- controllers/products.js
+++ controllers/products.js
@@ -33,7 +33,7 @@ const getAllProducts = async (req, res) => {
     console.log(queryObject);
 
     // const Products = await apiData;
-    res.status(200).json({Products, bbHits: Products.length });
+    res.status(200).json({apiData, bbHits: apiData.length });
 };
 
 const getAllProductsTesting = async (req, res) => {
