ff1e8bacf7184b1635f5ef042743a0f78d94d42e
shiwanshu pandey <48207289+shiwanshupandey@users.noreply.github.com>
2023-08-21 19:14:56 +0530

    Update products.js

--- controllers/products.js
+++ controllers/products.js
@@ -17,7 +17,7 @@ const getAllProducts = async (req, res) => {
         queryObject.name = { $regex: name, $options: "i" };
     }
 
-    if (id){
+    if(id){
         queryObject.id = id;
     }
 
@@ -34,10 +34,19 @@ const getAllProducts = async (req, res) => {
     apiData = apiData.select(selectFix);
   }
 
-    console.log(queryObject);
+  let page = Number(req.query.page) || 1;
+  let limit = Number(req.query.limit) || 10;
+
+  let skip = (page - 1) * limit;
+
+  // page = 2;
+  // limit = 3;
+  // skip =  1 * 3 = 3
+
+  apiData = apiData.skip(skip).limit(limit);
 
     const Products = await apiData;
-    res.status(200).json({Products});
+    res.status(200).json({Products, nbHits: Products.length });
 };
 
 const getAllProductsTesting = async (req, res) => {
